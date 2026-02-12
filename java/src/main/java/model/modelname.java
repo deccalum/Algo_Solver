109ae712b6005934edf309a1fc2fcfff3a2ff797
Dennis Calmius <dcalmius@gmail.com>
2026-02-12 12:42:56 +0100

    add modelname class skeleton

---
+++ java/src/main/java/model/modelname.java
@@ -0,0 +1,17 @@
+package model;
+
+/*
+@Entity
+@Data
+
+
+public class modelname {
+
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+ 
+    Map<String, Object> data = new HashMap<>();
+    data.put("key1", "value1");
+    data.put("key2", "value2");
+}
+*/
